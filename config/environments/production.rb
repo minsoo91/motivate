c2ac9d55bd9e2b49cba48b405be8c7ab4e46f535
Minsoo <minsoo91@gmail.com>
2014-12-23 20:59:22 -0800

    Set assets precompile on initialize to false in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
-
+  config.assets.initialize_on_precompile = false
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
